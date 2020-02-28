09a47a442aff8741a9f7a2ff7d9d35e421021716
Lucas Chaufournier <lucaschaufournier@gmail.com>
2020-02-28 17:15:43 -0500

    Revert "Introduction to Node.js Event Loop"

--- wiki/nodejs/async.js
+++
@@ -1,7 +0,0 @@
-console.log('Hi! This is a demo of async programming.');
-
-setTimeout(() => {
-    console.log("Hello from callback of set time out!")
-}, 5000);
-
-console.log("Set time out is not blocked!");
